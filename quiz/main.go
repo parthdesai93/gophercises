028708e0dbacce61bcc79e025123f803ca8cb89f
Parth Desai <psdesai.93@gmail.com>
2018-07-11 00:23:55 -0400

    read a csv file and ask a quiz

---
+++ quiz/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type problem struct {
+	q string
+	a string
+}
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file")
+	flag.Parse()
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Couldn't open the file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("failed to parse the csv file")
+	}
+	problems := parseLines(lines)
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("problem %d: %s \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("Correct answers: %d / %d", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+//sample comment
+func exit(msg string) {
+	fmt.Printf(msg)
+	os.Exit(1)
+}
